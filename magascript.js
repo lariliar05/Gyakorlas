b541c82c02754c8351f45f777bd0b042cbd9be1b
Zoltán Dániel Török <zdtorok@Zoltans-MacBook-Pro.local>
2025-08-23 18:13:33 +0200

    fixed syntax

--- magascript.js
+++ magascript.js
@@ -1,6 +1,6 @@
-document.getElementById("Bejelentgomb").addEventListener("click",function(){
+document.getElementById("regisztralgomb").addEventListener("click", function() {
 
-    const felhasznalok ={
+    const felhasznalok = {
         vezeteknev: document.getElementById("vezeteknev").value,
         keresztnev: document.getElementById("keresztesnev").value,
         email: document.getElementById("email").value,
@@ -10,32 +10,32 @@ document.getElementById("Bejelentgomb").addEventListener("click",function(){
         nem: document.querySelector('input[name="nem"]:checked').value,
     };
 
-if(!felhasznalok.vezeteknev || !felhasznalok.keresztnev || !felhasznalok.email || !felhasznalok.jelszo || !felhasznalok.szuldat || !felhasznalok.elfogad || !felhasznalok.nem){
-    alert("Kérem töltse ki az összes mezőt és fogadja el a feltételeket!");
-    return;
+    if(!felhasznalok.vezeteknev || !felhasznalok.keresztnev || !felhasznalok.email || !felhasznalok.jelszo || !felhasznalok.szuldat || !felhasznalok.elfogad || !felhasznalok.nem) {
+        alert("Kérem töltse ki az összes mezőt és fogadja el a feltételeket!");
+        return;
+    };
+    localStorage.setItem(felhasznalok.email, JSON.stringify(felhasznalok));
+    alert("Sikeres regisztráció!");
+    window.location.href = "Maga.html";
+});
 
-};
-localStorage.setItem(felhasznalok.email, JSON.stringify(felhasznalok));
-alert("Sikeres regisztráció!");
-window.location.href = "Maga.html";
 
-
-document.getElementById("Bejelentgomb").addEventListener("click",function(){
+document.getElementById("Bejelentgomb").addEventListener("click", function(){
     const email = document.getElementById("Bejelentemail").value;
     const jelszo = document.getElementById("jelsz").value;
     const felhasznalokadat = localStorage.getItem(email);
-    if(!felhasznalokadat){
+    if (!felhasznalokadat) {
         alert("Nincs ilyen Email regisztrálva!");
         return;
     }
+
     const felhasznalo = JSON.parse(felhasznalokadat);
 
-    if(felhasznalo.jelszo === jelszo){
+    if (felhasznalo.jelszo === jelszo) {
         alert("Sikeres bejelentkezés!");
         window.location.href = "fooldal.html";
-
-    } else{
+    } else {
         alert("Hibás jelszó!");
         return;
-}
-};
+    }
+});
